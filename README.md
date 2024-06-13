9d403d80d3c8f9562e3e8bd96987274f6bb5db4c
mohan2020coder <mohan18.welcome@gmail.com>
2024-06-13 11:53:20 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# codetracker-php
+# mongo-demo-php
+checking mongo in php
+
+run the docker
+
+#php -S localhost:8080 
+
+
+PHP 7.2.34 Development Server started at Wed Jun 12 16:47:14 2024
+
+Listening on http://localhost:8080
+
+
+Access in the browser>> http://localhost:8080/connection.php
+
+
+Connected to MongoDB!
+
+
+if connection is successful 
+
+Access in the browser>> 
+
+http://localhost:8080/index.php 
+
+
+http://localhost:8080
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/mongo-demo-php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f1c8d401d2d6749bc0d685db84727d892817e2b7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mongodb/mongodb' => array(
+            'pretty_version' => '1.6.1',
+            'version' => '1.6.1.0',
+            'reference' => '4bb040c620ab95c487d05d5ff50cbc92fa68e920',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mongodb/mongodb',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vendor/mongo-demo-php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f1c8d401d2d6749bc0d685db84727d892817e2b7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
